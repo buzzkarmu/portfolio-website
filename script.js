d80b70917cedb4639d686365ef2fb4fb1772cdd6
Buzz karmacharya <karmacharyabuzz@gmail.com>
2023-04-06 20:32:42 +0545

    modified

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed('.auto',{
-    strings: ['A Reader...','A Doer...','A Good Learner...'],
+    strings: ['A Reader...','A Doer...','A Learner...'],
     typeSpeed:100,
     backSpeed:100,
     loop: true
